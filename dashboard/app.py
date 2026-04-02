d72427d200121abff5fbf54d46fdf3c990cfc9b9
tharun2595-oss <tharun2595@gmail.com>
2026-04-02 15:44:32 +0530

    Add files via upload

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/crypto_prices.csv")
+
+st.title(" Crypto Tracker Dashboard")
+
+st.metric("Latest BTC", df['bitcoin'].iloc[-1])
+st.metric("Latest ETH", df['ethereum'].iloc[-1])
+
+st.line_chart(df[['bitcoin', 'ethereum']])
